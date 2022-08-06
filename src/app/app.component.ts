059a0d90ba3d76172a164cd94a357219fffd0bcd
Tharit <tharit@bonmek.com>
2022-08-06 09:27:51 +0700

    add ngOninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-rebase';
   constructor(){
     console.log("test create")
+  }
+
+  ngOnInit(): void {
+    console.log("test ng onit")
   }
 }
